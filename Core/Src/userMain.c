d39164b108a9714dc831efe431d0bdff85d517d6
Miron <oskroba.miron@gmail.com>
2021-04-19 09:10:27 +0200

    added UART_IT 10Hz transmission 115200br,cs8,ixon

--- Core/Src/userMain.c
+++ Core/Src/userMain.c
@@ -20,7 +20,7 @@ void userMain()
 	init_TIM_IT(&htim4);
 
 	// welcome message
-//	HAL_UART_Transmit_IT(&huart2, message_welcome, strlen(message_welcome));
+	HAL_UART_Transmit_IT(&huart2, message_welcome, (uint8_t)strlen(message_welcome));
 }
 
 void init_TIM_IT(TIM_HandleTypeDef *timer)
@@ -33,19 +33,20 @@ void w8(int delay)
 	HAL_Delay(delay);
 }
 
-uint16_t msg_len = 40;
+uint16_t msg_len = 80;
 
 void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim)	// 10Hz
 {
 
-	if(counter_IT % 5 == 0 && htim->Instance == TIM4)		// 1Hz
+	if(counter_IT % 1 == 0 && htim->Instance == TIM4)		// 10Hz
 	{
-		// allocate memory for message
-		uint8_t msg[40];
+		uint8_t *msg = (uint8_t*)calloc(msg_len, sizeof(uint8_t));
 
 		// create & transmit message
-		sprintf(msg, "wiadoomosc nr %d.\n\r", message_counter);
-		HAL_UART_Transmit_IT(&huart2, msg, 40);
+		msg_len = sprintf(msg, "Ty piszesz program, a Lewandowski strzelil juz %d goli.\r\n", message_counter);
+		HAL_UART_Transmit_IT(&huart2, msg, msg_len);
+
+		free(msg);
 
 		// blink
 		HAL_GPIO_TogglePin(GPIOC, GPIO_PIN_13);
